0e5f0039847b43856a800b522e79808b1a7c1cc4
V. Osório <flamingvpo@gmail.com>
2020-02-27 18:05:44 -0300

    27/02

--- src/views/List.vue
+++ src/views/List.vue
@@ -3,22 +3,24 @@
         <div class="page-title">
             <h1>Movies</h1>
         </div>
-        <div class="loading">
+        <div v-if="isLoading" class="loading">
             <h1>Loading...</h1>
         </div>
-        <div class="movie-list">
+        <div v-if="!isLoading" class="movie-list">
             <table>
                 <thead>
-                    <th>Title</th>
-                    <th>Genre</th>
-                    <th>Score</th>
-                    <th>Details</th>
+                    <th>#</th>
+                    <th>TITLE</th>
+                    <th>GENRE</th>
+                    <th>SCORE</th>
+                    <th>DETAILS</th>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Run</td>
-                        <td>Thriller</td>
-                        <td>9/10</td>
+                    <tr v-for="movie in movieList" v-bind:key="movie.id">
+                        <td>{{movie.id}}</td>
+                        <td>{{movie.title}}</td>
+                        <td>{{movie.genre}}</td>
+                        <td>{{movie.score}}</td>
                         <td>More...</td>
                     </tr>
                 </tbody>
@@ -28,31 +30,69 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: "list",
     data: function(){
         return {
-            title: '',
-
+            movieList: [],
+            hasErrors: false,
+            isLoading: false
         }
+    },
+    methods:{
+
+    },
+    created: function() {
+        this.isLoading = true
+        axios.get('http://5e48a84b728fde0014e34e74.mockapi.io/movies')
+            .then(response => {
+                response.data.forEach(movie => {
+                    this.movieList.push(movie)    
+                });
+                this.isLoading = false
+            }).catch(err => {
+                console.log(err)
+                this.isLoading = false
+            })
     }
 }
 </script>
 
 <style scoped>
     table{
-        margin-left: 20%;
-        margin-right: 20%;
-        width: 60%;
+        margin-left: 15%;
+        margin-right: 15%;
+        width: 70%;
         text-align: center;
+        margin-bottom: 5%;
+        background-color: black
+    }
+
+    td{
+        background-color: white
+    }
+
+    td{
+        font-weight: bold;
     }
 
     th{
-        font-size: 20px;
+        font-size: 25px;
+        color: white;
     }
 
     table, th, td{
         border: 1px solid black;
+        padding-top: 3px;
+        padding-bottom: 3px;
+    }
+
+    .page-title{
+        text-align: center;
+        margin-right: 20%;
+        margin-left: 20%;
     }
 
 </style>
